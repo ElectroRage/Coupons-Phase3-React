cd0246951cf5ec4c76b5869f47616147afb07270
Ellay's PC <ellay13@Gmail.com>
2024-12-25 00:22:33 +0200

    Lates Stages of Company Comp developments, most features are done, need to finish the coupon card.

--- src/Components/Router/Router.tsx
+++ src/Components/Router/Router.tsx
@@ -1,18 +1,44 @@
 import "./Router.css";
-import {Route, Routes} from "react-router-dom";
-import {LoginArea} from "../LoginArea/LoginArea";
-import {LoginBox} from "../LoginBox/LoginBox";
+import {Route, Routes, useLocation, useNavigate} from "react-router-dom";
+import {LoginBox} from "../LoginPage/LoginBox/LoginBox";
 import {AdminPage} from "../AdminComponents/AdminPage/AdminPage";
-import {AllCompanysComp} from "../AdminComponents/AllCompanysComp/AllCompanysComp";
 import {CompanyPanel} from "../CompanyComponents/CompanyPanel/CompanyPanel";
+import React, {useEffect, useState} from "react";
+import authService from "../../Services/AuthService";
+
 
 export function Router(): JSX.Element {
+    // const location = useLocation();
+    // const navigate = useNavigate();
+    //
+    // useEffect(() => {
+    //     const token = localStorage.getItem("token")
+    //     const email = localStorage.getItem("email")
+    //     if (!token || !email) {
+    //         alert("Missing Info, Please login")
+    //         // navigate("/login")
+    //         return;
+    //     }
+    //     authService.validate(token, email)
+    //         .then(bool => {
+    //             if (!bool) {
+    //                 localStorage.removeItem("token")
+    //                 alert("Session token expired, please login")
+    //                 // navigate("/login")
+    //                 return
+    //             }
+    //
+    //         })
+    //
+    //
+    // }, [location]);
+
     return (
         <div className="Routing">
             <Routes>
                 <Route path="login" element={<LoginBox/>}/>
-                <Route path="adminpanel" element={<AdminPage/>}/>
-                <Route path={"companypanel"} element={<CompanyPanel/>}/>
+                <Route path="administratorpanel" element={<AdminPage/>}/>
+                <Route path="companypanel" element={<CompanyPanel/>}/>
             </Routes>
 
         </div>
